6342078d55450f5b421bcbba63dcd29075840a89
mashharuki <avp.104.106.107@ae.auone-net.jp>
2020-08-09 19:59:19 +0900

    Initial Commit

---
+++ TestApps_for_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApps_for_iOS
+//
+//  Created by 近藤晴輝 on 2020/08/09.
+//  Copyright © 2020 近藤晴輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

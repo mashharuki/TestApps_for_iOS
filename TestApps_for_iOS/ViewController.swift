6c94622e6073c62f86239123f477672c418fd027
mashharuki <avp.104.106.107@ae.auone-net.jp>
2020-08-10 15:49:48 +0900

    スライドバーをクリックすると、割合が表示されるようにコーディング。

--- TestApps_for_iOS/ViewController.swift
+++ TestApps_for_iOS/ViewController.swift
@@ -13,11 +13,29 @@ import UIKit
  */
 class ViewController: UIViewController {
 
+    
+    /**ボタンと接続*/
+    @IBOutlet weak var label: UILabel!
+    
+    /**
+     * スライドバーと接続
+     */
+    @IBAction func showValue(_ sender: UISlider) {
+        label.text = "\(sender.value)"
+    }
+    
+    /**
+     * デフォルト処理
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    /**
+     * メモリ不足の際に呼び出す処理
+     */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 

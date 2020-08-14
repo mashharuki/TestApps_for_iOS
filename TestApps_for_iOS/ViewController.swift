fe8d502638c84a331008cfd72d2e5675474e7d13
mashharuki <avp.104.106.107@ae.auone-net.jp>
2020-08-14 21:49:19 +0900

    コメントを追加。

--- TestApps_for_iOS/ViewController.swift
+++ TestApps_for_iOS/ViewController.swift
@@ -20,13 +20,16 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
      * ツールバーのカメラアイコンを押した時に処理するメソッド
      */
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
-        //カメラの要素を取得
+        //画像の取得方法を変数に格納する。　ここでは、カメラロールの画像が表示されるように設定。
         let camera = UIImagePickerController.SourceType.camera
-        //
+        //カメラのないデバイスでは実行されないようにチェックしている。
         if UIImagePickerController.isSourceTypeAvailable(camera){
+            //カメラの画像を表示するためにモーダルビューコントローラーを作成する。
             let picker = UIImagePickerController()
+            //cameraをモーダルコントローラーに設定する。
             picker.sourceType = camera
             picker.delegate = self
+            //モーダルビューコントローラーを前面に出して、カメラ画面を表示する。
             self.present(picker,animated: true)
         }
     }
@@ -35,9 +38,13 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
      * ユーザーが写真を撮影し終えた時に処理するメソッド
      */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //infoからオリジナルの画像を取り出す。
         let image = info[.originalImage] as! UIImage
+        //イメージビューに画像を表示する。
         self.imageView.image = image
+        //取得した画像をアルバムに保存する。
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        //モーダルビューコントローラーを消去して、元の画像を表示する。
         self.dismiss(animated: true)
     }
     

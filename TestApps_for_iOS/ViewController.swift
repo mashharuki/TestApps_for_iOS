a2e99fd35e4f84d23bc638b5461b3ebbe91bcbab
mashharuki <avp.104.106.107@ae.auone-net.jp>
2020-08-14 20:50:24 +0900

    一部うまくいかない処理があるが、大体できているので、一旦コミットします。

--- TestApps_for_iOS/ViewController.swift
+++ TestApps_for_iOS/ViewController.swift
@@ -13,6 +13,19 @@ import UIKit
  */
 class ViewController: UIViewController {
 
+    /** 画像と接続 */
+    @IBOutlet weak var imageView: UIImageView!
+    
+    /**
+     * ツールバーと接続
+     */
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        //変数controllerを用意
+       let controller = UIActivityViewController(activityItems : [ imageView.image! ] ,applicationActivities : nil)
+       //モーダルビューコントローラーとして表示 self:自分自身のインスタンス
+        self.present(controller,animated: true,completion: {print("表示完了！")} )
+    }
+    
     /**
      * デフォルトで呼び出されるメソッド
      */

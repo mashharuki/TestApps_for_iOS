0f19800602b324b761cacc3fbe7569d5f345cde2
mashharuki <avp.104.106.107@ae.auone-net.jp>
2020-08-16 18:20:28 +0900

    クイズ5問を管理するクラスを追加しました。

---
+++ TestApps_for_iOS/QuizManager.swift
@@ -0,0 +1,56 @@
+//
+//  QuizManager.swift
+//  TestApps_for_iOS
+//
+//  Created by 近藤晴輝 on 2020/08/16.
+//  Copyright © 2020 近藤晴輝. All rights reserved.
+//
+
+import UIKit
+
+/**
+ * クイズ5問を管理するクラス
+ */
+class QuizManager{
+    //クイズ5問を格納するための配列
+    var quizzes: [Quiz]
+    //現在回答しているクイズの設問番号を格納する変数
+    var currentIndex : Int
+    //スコアを格納するための変数
+    var score: Int
+    
+    /**
+     * 回答状況を格納する列挙型変数
+     */
+    enum Status {
+        //回答中
+        case inAnswer
+        //回答終了
+        case done
+    }
+    //回答状況を格納する変数
+    var status : Status
+    
+    /**
+     * 初期化処理用メソッド
+     */
+    init() {
+        //配列を初期化
+        quizzes = []
+        //問題文、正解、画像名、でクイズを作成
+        //問１
+        quizzes.append(Quiz(text:"人間を超でっかいネコだと思っている？",correctAnswer: true,imageName: "cat"))
+        //問２
+        quizzes.append(Quiz(text:"犬は食べ物の美味しさを味よりも匂いで判断している？",correctAnswer: true,imageName: "dog"))
+        //問３
+        quizzes.append(Quiz(text:"トラのしましま模様は皮膚まで繋がっていない？",correctAnswer: true,imageName: "tiger"))
+        //問４
+        quizzes.append(Quiz(text:"クマは走る時に全部の足がバラバラに動いている？",correctAnswer: true,imageName: "bear"))
+        //問５
+        quizzes.append(Quiz(text:"パンダの一番の好物は、笹である？",correctAnswer: true,imageName: "panda"))
+        //各変数を初期化
+        currentIndex = 0
+        score = 0
+        status = .inAnswer
+    }
+}
